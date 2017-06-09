2af184d67ee551096cbd1b8872f5cb3b1e77c49c
inbar <gitemy13@gmail.com>
2017-06-09 17:47:29 +0300

    inbar

--- src/projectsalmon/Teacher.java
+++ src/projectsalmon/Teacher.java
@@ -1,16 +1,20 @@
 package projectsalmon;
 
+import java.util.Calendar;
+
 public class Teacher extends User{
 
 	private String degree;
 	private float weekly_hours;
 	private float maximal_weekly_hours;
+	private Calendar schedule;
 	
 	public Teacher(int max_hours,String id,String first, String last, String password)
 	{
 		super(id,first,last,password);
 		maximal_weekly_hours = max_hours;
 		weekly_hours = 0;
+		schedule = Calendar.getInstance();
 	}
 	
 	
